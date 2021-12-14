8191bf9b6b07116ceefe2c556a2cb04ac4eb9c95
DShulkin <thedavidshulkin@gmail.com>
2021-12-14 01:03:42 -0500

    uploaded

---
+++ app.js
@@ -0,0 +1,59 @@
+const phrases = ['Lucius Seneca', 'Marcus Aurelius', 'Momento Mori', 'Friedrich Nietzsche', 'Amor Fati']
+const qwerty = document.querySelector('#qwerty')
+const ul = phrase.querySelector('ul')
+let missed = 0
+
+const startGame = document.querySelector('.btn__reset')
+startGame.addEventListener('click', () => {
+   const overlay = document.querySelector('#overlay')
+   overlay.style.display = 'none'
+})
+
+const getRandomPhraseAsArray = (array) => {
+  let randomPhrase = array[Math.floor(Math.random() * phrases.length)]
+  let splitPhrase = randomPhrase.split('')  
+  return splitPhrase
+}
+
+const addPhraseToDisplay = (array) => {
+  const phraseArray = getRandomPhraseAsArray(array)
+  phraseArray.forEach(function(letter) {
+    const li = document.createElement('li')
+    li.textContent = letter
+    ul.appendChild(li)
+    if (li.textContent !=' ') {li.className = 'letter'} else {li.className = 'space'} 
+})
+}
+addPhraseToDisplay(phrases)
+
+
+const checkLetter = (button) => {
+  let match = null
+  const letters = ul.querySelectorAll('.letter')
+  letters.forEach(function(li) {
+    if (button.textContent == li.textContent) {
+      li.classList.add('show')
+      li.style.transition = '0.5s ease-in-out'
+      match = button.textContent
+    } 
+
+  })
+  return(match)
+}
+
+qwerty.addEventListener('click', (e) => {
+  const button = e.target
+  if (button.tagName === 'BUTTON' || button.className === 'chosen') {
+   
+    button.className = 'chosen'
+    button.disabled = true
+    console.log(button.textContent, 'CLICKED')
+
+    const letterFound = checkLetter(button)
+    console.log(letterFound, 'FOUND')
+
+      if (letterFound === null || letterFound != button.textContent) { 
+       console.log(letterFound, 'NOT INCLUDED')
+      }
+  }
+})
